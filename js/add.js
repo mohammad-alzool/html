919a26afd5f90685572f92b0f64628c9c837ef40
mohammad-alzool <m.zool@outlook.com>
2021-01-28 13:37:31 +0200

    1st test

--- js/add.js
+++ js/add.js
@@ -1,20 +1,41 @@
-var addetion=prompt("how do you want your pizza today?");
+var addetion=prompt("how do you want your pizza today (VEGETARIAN or PEPPERONI)?");
+
+
+while (addetion !== 'PEPPERONI' && addetion !== 'VEGETARIAN') {
+  addetion = prompt('you can choose between two items (VEGETARIAN or PEPPERONI)');
+}
+
 var noofp=prompt("how many pizza do you want?");
 
 var orderDetail;
+
+
+  var orderDetail;
 
 if (noofp > 1 && noofp < 3){
   orderDetail="Congrats you have 10% discount on your order!";
 } else if (noofp > 3 && noofp < 6){
   orderDetail="Congrats you have 20% discount and free pepsi";
-} else if (noofp > 7 && noofp < 9999999){
+} else if (noofp > 6 && noofp < 9999999){
   orderDetail="Congrats you have 50% discount and free pepsi";
 } else {
   orderDetail="somthing wrong with your order!!!";
 }
 
+var itemimeg = '';
+
+if (addetion === 'PEPPERONI') {
+  itemimeg = '<img src="/img/PEPPERONI.jpg"/>';
+} else if (addetion === 'VEGETARIAN') {
+  itemimeg = '<img src="/img/VEGETARIAN.jpg"/>';
+}
+
+  document.write(itemimeg);
+
 document.write(orderDetail);
 document.getElementById("orderDetail").innerText=addetion
+
+
 
 document.getElementById("noofp").innerText=noofp
 
