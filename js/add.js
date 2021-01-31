58cdee87a60a208fa5835a8b2f3c63c826140335
mohammad-alzool <m.zool@outlook.com>
2021-01-31 19:29:38 +0200

    add function

--- js/add.js
+++ js/add.js
@@ -5,12 +5,37 @@ while (addetion !== 'PEPPERONI' && addetion !== 'VEGETARIAN') {
   addetion = prompt('you can choose between two items (VEGETARIAN or PEPPERONI)');
 }
 
-var noofp=prompt("how many pizza do you want?");
+
+
+
+var noofp;
+
+function cnumber ()
+{
+  while (noofp=prompt("how many pizza do you want?" , "please enter Number only") ) {
+    if (isNaN(noofp)) {
+      alert("Invalid input.")
+      ;
+    } else {
+      return parseInt(noofp);
+    }
+  }
+
+
+}
+if (noofp >= 1 && noofp <= 3){
+  orderDetail="Congrats you have 10% discount on your order!";
+} else if (noofp >= 4 && noofp <= 6){
+  orderDetail="Congrats you have 20% discount and free pepsi";
+} else if (noofp >= 7 && noofp <= 9999999){
+  orderDetail="Congrats you have 50% discount and free pepsi";
+} else { 
+  cnumber();
+}
 
 var orderDetail;
 
 var itemimeg = '';
-
 if (addetion === 'PEPPERONI') {
   itemimeg = '<img src="PEPPERONI.jpg"/>';
 } else if (addetion === 'VEGETARIAN') {
@@ -22,26 +47,17 @@ for (var i = 0 ; i < noofp ; i++){
   result=result+itemimeg;
 
 }
+
   document.write(itemimeg);
 
 
-if (noofp >= 1 && noofp <= 3){
-  orderDetail="Congrats you have 10% discount on your order!";
-} else if (noofp >= 4 && noofp <= 6){
-  orderDetail="Congrats you have 20% discount and free pepsi";
-} else if (noofp >= 7 && noofp <= 9999999){
-  orderDetail="Congrats you have 50% discount and free pepsi";
-} else {
-  orderDetail="somthing wrong with your order!!!";
-}
 
 
-document.write(result);
+document.write("number of pizza by imege" + result );
 document.write(orderDetail);
+
+
 document.getElementById("orderDetail").innerText=addetion
-
-
-
 document.getElementById("noofp").innerText=noofp
 
 confirm("number of of pizza :" + noofp)
